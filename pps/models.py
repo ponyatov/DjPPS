bbeff3c54e9d754a7f51528ab66d88c9ed0a215b
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-09-17 11:02:03 +0400

    + pps.Task
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- pps/models.py
+++ pps/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+	title = models.CharField(max_length=77)
+	notes = models.TextField()
+	created = models.DateTimeField()
+
+from django.contrib import admin
+
+admin.site.register(Task)
+
